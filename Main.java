4975d68360599e1cdc37ba63bfbcfb8b06c2caee
stacey.chen24 <stacey.chen24@d155.org>
2024-02-16 11:25:02 -0600

    phase two dome

--- Main.java
+++ Main.java
@@ -26,16 +26,40 @@ public class Main
         Yorkshire yorkA = new Yorkshire("Yarn", 5);
 
        ArrayList <Dog> dogs = new ArrayList <Dog>();
+        dogs.add(0,labA);
+        dogs.add(1,yorkA);
+        dogs.add(2,dogA);
+
         Dog[] triDogs = {dogA, labA, yorkA};
+
+
+        ((Labrador)triDogs[1]).waddle();
+//I had to cast the Labrador to waddle because it's a Lab stored in Dog,
+//so the system looks in Dog to find waddle, but it doesn't exist in the Dog class
+//But bark does exist in the Dog class so the code was able to compile properly
+
+        for(int i = 0; i < dogs.size(); i++){
+            System.out.println("Arraylist doggoss:" + dogs.get(i).bark());
+
+        }//Arraylist for loop
+
+        for (Dog dog : dogs) {
+            System.out.println(dog.bark());
+    }//Arraylist enhanced
+
+
+
+
+
+
 
     for (int i = 0; i < 3; i ++){
         System.out.println("For " + triDogs[i].getName() + " says " + triDogs[i].bark());
-
-    }
+    }//regular for array
 
     for(Dog dog: triDogs){
         System.out.println("For each " + dog.getName() + " ays " + dog.bark());
-    }
+    }//for each array
 
 	    System.out.println(dDog.getName() + " says " + dDog.bark());
         System.out.println(lDoggy.getName() + " says " + lDoggy.bark());
